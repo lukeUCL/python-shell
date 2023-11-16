ddeea63ddd675368a241c7865d9baac874e1574a
IZ-code136 <isaacpf12345@gmail.com>
2023-11-16 17:52:27 +0000

    fixed errors on initial separation

--- src/shell.py
+++ src/shell.py
@@ -25,28 +25,7 @@ def eval(cmdline, out):
                     tokens.append(m.group(0))
         app = tokens[0]
         args = tokens[1:]
-        apps(app, args)
-
-
-if __name__ == "__main__":
-    args_num = len(sys.argv) - 1
-    if args_num > 0:
-        if args_num != 2:
-            raise ValueError("wrong number of command line arguments")
-        if sys.argv[1] != "-c":
-            raise ValueError(f"unexpected command line argument {sys.argv[1]}")
-        out = deque()
-        eval(sys.argv[2], out)
-        while len(out) > 0:
-            print(out.popleft(), end="")
-    else:
-        while True:
-            print(os.getcwd() + "> ", end="")
-            cmdline = input()
-            out = deque()
-            eval(cmdline, out)
-            while len(out) > 0:
-                print(out.popleft(), end="")
+        apps(app, args, out)
 
 def apps(app, args, out):
     if app == "pwd":
@@ -121,5 +100,23 @@ def apps(app, args, out):
     else:
         raise ValueError(f"unsupported application {app}")
 
-
+if __name__ == "__main__":
+    args_num = len(sys.argv) - 1
+    if args_num > 0:
+        if args_num != 2:
+            raise ValueError("wrong number of command line arguments")
+        if sys.argv[1] != "-c":
+            raise ValueError(f"unexpected command line argument {sys.argv[1]}")
+        out = deque()
+        eval(sys.argv[2], out)
+        while len(out) > 0:
+            print(out.popleft(), end="")
+    else:
+        while True:
+            print(os.getcwd() + "> ", end="")
+            cmdline = input()
+            out = deque()
+            eval(cmdline, out)
+            while len(out) > 0:
+                print(out.popleft(), end="")
 
