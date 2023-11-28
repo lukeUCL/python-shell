a6b9dafdfbdb4fa5918cc31c03f3aefd6d720204
“lukemciver” <Cocoandlulu1>
2023-11-28 17:51:46 +0000

    quick fix

--- src/parseTesting.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def rawC(test):
-    raw_commands = re.findall(r"([^\"';]+|\"[^\"]*\"|'[^']+')",test)
-    print(raw_commands)
-
-# Test the function
-rawC('echo "Hello World"; ls -l \'My Folder\'')
